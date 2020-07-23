1b0bcd5d24e8f934ba856b88e1b4e49ac032959d
rubenaraya <raraya@masexperto.com>
2020-07-23 00:37:42 -0400

    Actualizacion

--- src/Bases/Ruteador.php
+++ src/Bases/Ruteador.php
@@ -11,20 +11,32 @@ abstract class Ruteador implements IRuteador
 	public $estados = array();
 
 	function __construct( $back = '', $front = '' ) {
-		if ( strlen($front)==0 ) { $front = $back; }
-		M::$entorno['RUTA']['BACKEND'] = $back;
-		M::$entorno['RUTA']['FRONTEND'] = $front;
+        if ( strlen($back)>0 ) {
+            if ( strlen($front)==0 ) { $front = $back; }
+            M::$entorno['RUTA']['BACKEND'] = $back;
+            M::$entorno['RUTA']['FRONTEND'] = $front;
+        }
+        $predeterminado = getcwd();
+        if ( !isset(M::$entorno['RUTA']['BACKEND']) ) {
+            M::$entorno['RUTA']['BACKEND'] = $predeterminado;
+        }
+        if ( !isset(M::$entorno['RUTA']['FRONTEND']) ) {
+            M::$entorno['RUTA']['FRONTEND'] = $predeterminado;
+        }
+        if ( !isset(M::$entorno['ALMACEN']['PUBLICO']) ) {
+            M::$entorno['ALMACEN']['PUBLICO'] = $predeterminado;
+        }
+        if ( !isset(M::$entorno['ALMACEN']['PRIVADO']) ) {
+            M::$entorno['ALMACEN']['PRIVADO'] = $predeterminado;
+        }
+        if ( !isset(M::$entorno['RUTA']['WEBME']) ) {
+            M::$entorno['RUTA']['WEBME'] = $predeterminado;
+        }
 		M::$entorno['RUTA']['BACKEND'] = str_replace( '\\', '/', realpath( M::$entorno['RUTA']['BACKEND'] ) );
 		M::$entorno['RUTA']['FRONTEND'] = str_replace( '\\', '/', realpath( M::$entorno['RUTA']['FRONTEND'] ) );
-		if ( isset(M::$entorno['ALMACEN']['PUBLICO']) ) {
-			M::$entorno['ALMACEN']['PUBLICO'] = str_replace( '\\', '/', realpath( M::$entorno['ALMACEN']['PUBLICO'] ) );
-		}
-		if ( isset(M::$entorno['ALMACEN']['PRIVADO']) ) {
-			M::$entorno['ALMACEN']['PRIVADO'] = str_replace( '\\', '/', realpath( M::$entorno['ALMACEN']['PRIVADO'] ) );
-		}
-		if ( isset(M::$entorno['RUTA']['WEBME']) ) {
-			M::$entorno['RUTA']['WEBME'] = str_replace( '\\', '/', realpath( M::$entorno['RUTA']['WEBME'] ) );
-		}
+        M::$entorno['ALMACEN']['PUBLICO'] = str_replace( '\\', '/', realpath( M::$entorno['ALMACEN']['PUBLICO'] ) );
+        M::$entorno['ALMACEN']['PRIVADO'] = str_replace( '\\', '/', realpath( M::$entorno['ALMACEN']['PRIVADO'] ) );
+        M::$entorno['RUTA']['WEBME'] = str_replace( '\\', '/', realpath( M::$entorno['RUTA']['WEBME'] ) );
         M::$entorno['RUTA']['PHPME'] = str_replace( '\\', '/', dirname(__DIR__) );
     }
 	function __destruct() {
